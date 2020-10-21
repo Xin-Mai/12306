7f84f0ef4d8658ed47c9584fa6e993301b0b6755
guopengyuan <3475556802@qq.com>
2020-10-21 23:32:13 +0800

    查询界面初实验

--- miniprogram/pages/search/search.js
+++ miniprogram/pages/search/search.js
@@ -1,10 +1,28 @@
 // pages/search/search.js
+
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
+    region1:['湖北省','武汉市','洪山区'],
+    region2:['湖北省','武汉市','洪山区'],
+    date: '2016-09-01',
+
+    bindRegionChange1: function (e) {
+      console.log('picker发送选择改变，携带值为', e.detail.value)
+      this.setData({
+        region1: e.detail.value
+      })
+    },
+
+    bindDateChange2: function(e) {
+      console.log('picker发送选择改变，携带值为', e.detail.value)
+      this.setData({
+        date: e.detail.value
+      })
+    }
 
   },
 
@@ -12,55 +30,55 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
-
+    
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-
+    
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
   onHide: function () {
-
+    
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
   onUnload: function () {
-
+    
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
   onPullDownRefresh: function () {
-
+    
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
   onReachBottom: function () {
-
+    
   },
 
   /**
    * 用户点击右上角分享
    */
   onShareAppMessage: function () {
-
+    
   }
 })
