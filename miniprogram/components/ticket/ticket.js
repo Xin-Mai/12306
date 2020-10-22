8e29c4e7d94fad8f642212302e148edd0b2abaa0
guopengyuan <3475556802@qq.com>
2020-10-22 21:04:32 +0800

    鹏远哭

---
+++ miniprogram/components/ticket/ticket.js
@@ -0,0 +1,34 @@
+// components/ticket.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+
+        from : String,
+        to : String,
+        fromTime : String,
+        toTime : String,
+        Id : String,
+        time : String,
+        ifSeat : Boolean,
+        ifHard : Boolean,
+        ifSoft : Boolean,
+        ifNoSeat : Boolean
+        
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+      
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  }
+})
